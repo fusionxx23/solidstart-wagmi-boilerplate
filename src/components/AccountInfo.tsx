0d3a96c0d8cf5dc1111048c9236c7195f80efe27
Tarp <travistarpley0@gmail.com>
2024-10-29 16:41:36 -0700

    feat: added hooks

--- src/components/AccountInfo.tsx
+++ src/components/AccountInfo.tsx
@@ -1,11 +1,19 @@
-import { createGetAccount } from "~/lib/wagmi-wrapper/getAccount";
+import { useGetAccount } from "~/lib/wagmi-wrapper/getAccount";
+import { useSimulateMintWeth } from "./hooks/useSimulateMintWeth";
+import { useGetConnector } from "~/lib/wagmi-wrapper/getConnector";
+import { initializeWagmiConfig } from "./hooks/useIsConnected";
 
 export default function AccountInfo() {
-  const account = createGetAccount();
+  const account = useGetAccount();
+  useSimulateMintWeth();
+
+  initializeWagmiConfig();
+  useGetConnector();
   return (
     <div class="">
       <div>
         <span>Address: {account().address}</span>
+        <span>ChainId: {account().chainId}</span>
       </div>
       <div>
         <span>isConnected: {account().isConnected ? "True" : "False"}</span>
