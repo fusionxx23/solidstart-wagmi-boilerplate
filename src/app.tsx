c90c8dc3d1e8b19f202b8707e83f2907298a76ce
Tarp <travistarpley0@gmail.com>
2024-10-29 16:40:51 -0700

    feat: Added Providers to app

--- src/app.tsx
+++ src/app.tsx
@@ -3,18 +3,32 @@ import { FileRoutes } from "@solidjs/start/router";
 import { Suspense } from "solid-js";
 import Nav from "~/components/Nav";
 import "./app.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/solid-query";
+import { AppKitProvider } from "./components/appKitProvider";
 
+import { clientOnly } from "@solidjs/start";
+const queryClient = new QueryClient();
+
+const ConfigProvider = clientOnly(
+  () => import("./lib/wagmi-wrapper/configProvider"),
+);
 export default function App() {
   return (
-    <Router
-      root={(props) => (
-        <>
-          <Nav />
-          <Suspense>{props.children}</Suspense>
-        </>
-      )}
-    >
-      <FileRoutes />
-    </Router>
+    <ConfigProvider>
+      <AppKitProvider>
+        <QueryClientProvider client={queryClient}>
+          <Router
+            root={(props) => (
+              <>
+                <Nav />
+                <Suspense>{props.children}</Suspense>
+              </>
+            )}
+          >
+            <FileRoutes />
+          </Router>
+        </QueryClientProvider>
+      </AppKitProvider>
+    </ConfigProvider>
   );
 }
